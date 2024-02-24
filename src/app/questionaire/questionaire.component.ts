cb35b627ef1cbfb76322e5e4166b5d728bbfef28
Sharik-oss <mamukabigvava2201@gmail.com>
2024-02-24 13:21:47 +0400

    added json file

--- src/app/questionaire/questionaire.component.ts
+++ src/app/questionaire/questionaire.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, NgModule } from '@angular/core';
-import { questionsInterface } from '../questionaire';
 import { CommonModule } from '@angular/common';
-
+import questions from "../json/questions.json"
 
 @Component({
   selector: 'app-questionaire',
@@ -10,71 +9,20 @@ import { CommonModule } from '@angular/common';
   templateUrl: './questionaire.component.html',
   styleUrl: './questionaire.component.scss'
 })
-export class QuestionaireComponent{
 
-  questions : questionsInterface[] = [
-    {
-      questionText: "vin var me?",
-      questionAnswers: [
-        "mamuka",
-        "tornike",
-        "irakli",
-        "giorgi"
-      ],
-      answer: "tornike",
-      id: 1
-    },
-    {
-      questionText: "vin var me?",
-      questionAnswers: [
-        "mamuka",
-        "tornike",
-        "irakli",
-        "giorgi"
-      ],
-      answer: "mamuka",
-      id: 2
-    },
-    {
-      questionText: "vin var me?",
-      questionAnswers: [
-        "mamuka",
-        "tornike",
-        "irakli",
-        "giorgi"
-      ],
-      answer: "giorgi",
-      id: 3
-    },
-    {
-      questionText: "vin var me?",
-      questionAnswers: [
-        "mamuka",
-        "tornike",
-        "irakli",
-        "giorgi"
-      ],
-      answer: "tornike",
-      id: 4
-    }, 
-    {
-      questionText: "vin var me?",
-      questionAnswers: [
-        "mamuka",
-        "tornike",
-        "irakli",
-        "giorgi"
-      ],
-      answer: "mamuka",
-      id: 5
-    }    
-  ]
+export class QuestionaireComponent implements OnInit{
+  i : number = 0
+  question : any = questions[this.i];
   score: any = 0;
+  ngOnInit(): void {
+    console.log(this.question);
+    
+  }
 
 
   checkAnswer(event: Event, answer: string,index: number){
-    console.log(event, answer, this.questions[0].answer, index );
-    if(answer === this.questions[index].answer){
+    console.log(this.question[0].answer);
+    if(answer === this.question[index].answer){
       console.log(true);
     }else{
       console.log(false);      
